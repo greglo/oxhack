35a246bee3003a81b725511d84e61634b40eb12d
Lukas Köbis <lukaskoebis@gmail.com>
2014-11-30 11:34:11 +0000

    rc2

--- public/app.js
+++ public/app.js
@@ -3,7 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'ngAnimate',
   'myApp.view1',
 ]).
 config(['$routeProvider', function($routeProvider) {
